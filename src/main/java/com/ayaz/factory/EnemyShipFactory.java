477631d88717ed0601e674878ea2c0b2c7a5031a
Ayaz <53002825+ayaznacafli@users.noreply.github.com>
2020-02-03 16:42:30 +0300

    I have created the factory pattern

---
+++ src/main/java/com/ayaz/factory/EnemyShipFactory.java
@@ -0,0 +1,15 @@
+package com.ayaz.factory;
+
+public class EnemyShipFactory {
+
+    public EnemyShip makeEnemyShip(String type){
+        if(type.equals("U")){
+            return new UFOEnemyShip();
+        }else if(type.equals("R")){
+            return new RocketEnemyShip();
+        }else if(type.equals("B")){
+            return new BigUFOEnemyShip();
+        }else
+            return null;
+    }
+}
