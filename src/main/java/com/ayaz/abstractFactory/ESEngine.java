96553a60463387c29085c3f9e25a09112a32564b
Ayaz <53002825+ayaznacafli@users.noreply.github.com>
2020-02-04 15:53:27 +0300

    I have created  Abstract Factory and Singleton pattern

---
+++ src/main/java/com/ayaz/abstractFactory/ESEngine.java
@@ -0,0 +1,5 @@
+package com.ayaz.abstractFactory;
+
+public interface ESEngine {
+    public String toString();
+}
---
+++ src/main/java/com/ayaz/abstractFactory/UFOEnemyShipFactory.java
@@ -0,0 +1,13 @@
+package com.ayaz.abstractFactory;
+
+public class UFOEnemyShipFactory implements EnemyShipFactory {
+    @Override
+    public ESWeapon addESGun() {
+        return new ESUFOGun();
+    }
+
+    @Override
+    public ESEngine addESEngine() {
+        return new ESUFOEngine();
+    }
+}
